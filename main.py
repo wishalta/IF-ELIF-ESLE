e39c84ab13ef62ba1ed78146552d311af2aba6dc
wishalta <rolandas.lebed@gmail.com>
2024-10-10 10:51:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+#pratesiu, ka per pirma pamoka nedadariau
+
+# 10
+
+# num1 = int(input('Iveskite jusu skaiciu: '))
+#
+# if num1 % 2 == 0:
+#     print('skaicius yra lyginis')
+# elif num1 == 3:
+#     print('skaicius lygus 3')
+# else:
+#     print(False)
+
+# 11
+#
+# a = int(input('Jusu 3 skaiciai: '))
+# b = int(input())
+# c = int(input())
+#
+# if a == b:
+#     print('pirmi du lygus')
+# elif a == c:
+#     print('pirmas ir trecias lygus')
+# elif c > a:
+#     print('trecias didesnis uz pirma')
+# elif (c*2) == b:
+#     print('ketvirtas ats')
+# elif a // 3:
+#     print('dalynasi')
+# else:
+#     print(False)
+
+# 12
+
+# a = int(input('Jusu 3 skaiciai: '))
+# b = int(input())
+# c = int(input())
+#
+# if a > b and a > c:
+#     print('pirmas didziausias')
+# elif b > c:
+#     print('antras didziausias')
+# elif c > b:
+#     print('trecias didziausias')
+
+# 13
+
+# a = int(input('Jusu 3 skaiciai: '))
+# b = int(input())
+# c = int(input())
+#
+# if a < b and a < c:
+#     print('pirmas maziausias')
+# elif b < c:
+#     print('antras maziausias')
+# else:
+#     print('trecias maziausias')
+#
+
+# 15
+
+d = int(input('Jusu 2 skaiciai: '))
+e = int(input())
+
+if d > e or d == 0:
+    print('pirma salyga')
+if e > d or e == 5:
+    print('antra salyga')
+if d > e and d == 20:
+    print('trecia salyga')
+if e > d and e < 100:
+    print('ketvirta salyga')
+
+git config --global user.email "rolandas.lebed@gmail.com"
+git config --global user.name "wishalta"
